d64b4afe7bffe7983c3acd1a35e832bd0fa4548b
daweizhuang <dawei.zhuang@antra.net>
2016-01-28 11:33:53 -0500

    second time

--- DemoProject/src/net/antra/daythree/CreatureKiller.java
+++ DemoProject/src/net/antra/daythree/CreatureKiller.java
@@ -2,6 +2,6 @@ package net.antra.daythree;
 
 public class CreatureKiller {
 	public static void kill(Creature aCreature){
-		System.out.println("a " + aCreature.getClass() + " is killed.");
+		System.out.println("a " + aCreature.getClass() + " is killed.");  
 	}
 }
